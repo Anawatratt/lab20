ec925c1c073a83559e6ef7c2f446087afe1d2f3d
Anawatratt <110977191+Anawatratt@users.noreply.github.com>
2023-02-21 16:55:07 +0700

    ส่งครับบ

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -1,12 +1,35 @@
 #include<iostream>
+
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R1,Rect R2){
+
+	double upper=min(R1.y,R2.y);
+	double lower=max(R1.y-R1.h,R2.y-R2.h);
+	double h=(upper-lower);
+
+	double yupper=max(R1.x,R2.x);
+	double ylower=min(R1.x+R1.w,R2.x+R2.w);
+	double w=(ylower-yupper);
+
+	if(h*w<=0){
+		return 0;
+	}
+	return (h*w);
+}
 
 
 
+
+
+
+int main(){
+	Rect R1 = {1,1,5,5};
+	Rect R2 = {2,2,5,5};
+
+	cout<<overlap(R1,R2);
 }
